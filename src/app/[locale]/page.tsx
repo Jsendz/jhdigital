443205aafedcfc08b89d0394149685f7d3d60acc
jsendz <joshuasendzul@gmail.com>
2025-02-04 19:32:29 +0100

    jhm

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,62 +5,26 @@ import Header from "../components/Header";
 import Testimonial from "../components/testimonials/Testimonials";
 import { getTranslations } from "next-intl/server";
 import { Metadata, ResolvingMetadata } from "next";
+import { ReactNode } from "react";
 
 
-
+type Props = {
+  children: ReactNode;
+  params: {locale: string};
+};
 
 
  
 // Define Props type for correct usage with Next.js
-type Props = {
-  params: Promise<{ locale: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-};
-
-// Dynamic SEO Metadata
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // Read route params
-  const locale = (await params).locale;
-
-  // Fetch translations
-  const t = await getTranslations({ locale, namespace: "Index" });
-
-  // Access and extend parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
+export async function generateMetadata({
+  params: {locale}
+}: Omit<Props, 'children'>) {
+  const t = await getTranslations({locale, namespace: 'Index'});
 
   return {
-    title: t("title"), // Localized Page Title
-    description: t("description"), // Localized Meta Description
-    alternates: {
-      canonical: `https://jhdigitalservices.com/${locale}`,
-    },
-    openGraph: {
-      title: t("title"),
-      description: t("description"),
-      url: `https://jhdigitalservices.com/${locale}`,
-      siteName: "My Website",
-      images: [
-        {
-          url: `https://jhdigitalservices.com/assets/${locale}/og-image.jpg`,
-          width: 1200,
-          height: 630,
-          alt: t("ogImageAlt"),
-        },
-      ],
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: t("title"),
-      description: t("description"),
-      images: [`https://jhdigitalservices.com/assets/${locale}/twitter-image.jpg`],
-    },
+    title: t('title')
   };
 }
-
 
 export default function IndexPage() {
 
