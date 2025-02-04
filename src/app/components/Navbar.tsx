bdae4dc99d77260f89e965b905fd78a09d65b505
jsendz <joshuasendzul@gmail.com>
2025-02-04 20:52:25 +0100

    jhm

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,17 +3,22 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
+import LocaleSwitcher from "./LocaleSwitcher";
+import Image from "next/image";
+import NavigationLink from "./NavigationLink";
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-900 text-white p-4 shadow-md" aria-label="Main Navigation">
+    <nav className="bg-white bg-opacity-70 p-4 shadow-md" aria-label="Main Navigation">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <Link href="/" className="text-xl font-bold cursor-pointer" aria-label="Homepage">
-          MyApp
-        </Link>
+        <NavigationLink href="/" className="text-xl font-bold cursor-pointer" aria-label="Homepage">
+        <Image src="/logo.svg" alt="founder's photo" width={100} height={100} className="" />
+          
+        </NavigationLink>
         
         {/* Mobile Menu Button */}
         <button
@@ -30,16 +35,20 @@ const Navbar = () => {
           role="menu"
         >
           <li role="menuitem">
-            <Link href="/about" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">About</Link>
+            <NavigationLink href="/web" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">About</NavigationLink>
           </li>
           <li role="menuitem">
-            <Link href="/services" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">Services</Link>
+            <NavigationLink href="/marketing" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">Services</NavigationLink>
           </li>
           <li role="menuitem">
-            <Link href="/contact" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">Contact</Link>
+            <NavigationLink href="/about" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">Contact</NavigationLink>
+          </li>
+          <li role="menuitem">
+            <NavigationLink href="/contact" className="block py-2 md:py-0 px-4 md:px-0 hover:text-gray-400">Contact</NavigationLink>
           </li>
         </ul>
       </div>
+      <LocaleSwitcher />
     </nav>
   );
 };
